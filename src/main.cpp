1d15856f937601d031940efdee572cfc822d1dd7
Benjamin Wolff <benjamin.wolff@digitecgalaxus.ch>
2025-06-22 11:04:48 +0200

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 int main(int /*argc*/, char * /*argv*/[]) {
+  // completion and ""-header insertion does NOT work
+  // after visiting helloworld.hpp, completion works but not ""-header insertion
   // helloWorld()
+
+  // std::cout // completion and <>-header insertion work
   return 0;
 }
